d8bce5c0c66b5b23d5e8b61a0f5028d8813b728e
BuildTools <unconfigured@null.spigotmc.org>
2019-10-09 19:25:00 -0600

    Bot now starts up

--- InfernalAdmin/bot.py
+++ InfernalAdmin/bot.py
@@ -1,14 +1,23 @@
-from discord import Client, Game, Message
+"""
+This is the main bot file
+"""
 import json
 
+from discord.ext import commands
 
-class InfernalAdminClient(Client):
+from bot_modules.welcome import Welcome
+
+
+class InfernalAdminClient(commands.Bot):
     """
     Main Infernal Admin Class
     This defines the main entry points for messages
     """
-    def __init__(self, loop, c_file):
-        super(InfernalAdminClient, self).__init__()
+
+    def __init__(self, c_file, *args, **kwargs):
+        super(InfernalAdminClient, self).__init__(*args, **kwargs)
+
+        self.add_cog(Welcome(self))
 
 
         with open(c_file) as f:
@@ -17,9 +26,11 @@ class InfernalAdminClient(Client):
 
         self.event(self.on_message)
         self.event(self.on_ready)
-        self.loop = loop
+        # self.bg_task = self.loop.create_task(self.my_background_task())
+        self.run(self.config['token'])
 
     async def on_ready(self):
+
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
