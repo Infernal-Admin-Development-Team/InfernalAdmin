2909796aadcd1a7f8be5e7313bcb768ac0f3d858
BuildTools <unconfigured@null.spigotmc.org>
2019-10-11 19:56:26 -0600

    added mopre updatew stuff

--- bot_modules/auto_update.py
+++ bot_modules/auto_update.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from pathlib import Path
 import os
 from github import Github
-
+from subprocess import check_call as run
 class AutoUpdate(commands.Cog):
     """This will contain everything relating to welcome"""
 
@@ -10,13 +10,23 @@ class AutoUpdate(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def update(self,ctx):
+    async def branches(self,ctx):
         """updates the bot"""
         #print("aaa")
         g=Github()
         repo = g.get_repo(self.bot.config['version-control']['repo'])
         branches=list(repo.get_branches())
         cwd = Path(os.getcwd())
+        out_str=" "
+        for b in branches:
+            out_str+=b.name+"\n "
+        await ctx.send("There are "+str(len(branches))+" branches\n```"+out_str+"```")
 
-        await ctx.send(str(branches))
+    @commands.command()
+    async def update(self,ctx,branch:str):
+        try:
+            await ctx.send("Updating to ``"+branch+"``")
+        except commands.errors.MissingRequiredArgument:
+            await ctx.send("Invalid arguements")
+
 
