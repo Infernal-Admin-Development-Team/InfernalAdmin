2735e42ddae0f0598f9af7ed237392b8c95a854b
BuildTools <pgibbs65@gmail.com>
2019-10-12 16:01:03 -0600

    Changed updater

--- update_windows.py
+++ update_windows.py
@@ -1,29 +1,51 @@
+"""Short and ugly script to install and re install the bot on windows"""
 import os
 import subprocess
+import sys
 from pathlib import Path
 
 cwd = Path(os.getcwd())
 parent = cwd.parent
 protected_files = []
 import shutil
+import stat
+
+
+def on_rm_error(func, path, exc_info):
+    # path contains the path of the file that couldn't be removed
+    # let's just assume that it's read-only and unlink it.
+    os.chmod(path, stat.S_IWRITE)
+    os.unlink(path)
 
 def copy_protected():
-    with open(os.path.join(os.getcwd(), 'InfernalAdmin/protected_files.txt')) as f:
-        protected_files = f.readlines()
-    f.close()
+    file_list = []
+    if Path(os.path.join(os.getcwd(), 'InfernalAdmin/protected_files.txt')).exists():
+        with open(os.path.join(os.getcwd(), 'InfernalAdmin/protected_files.txt')) as f:
+            file_list = f.readlines()
+        f.close()
+        for f in file_list:
+            protected_files.append(f.replace('\n', ""))
+        for f in protected_files:
+            shutil.copy2(os.path.join(os.getcwd(), "InfernalAdmin\\" + f), os.getcwd() + "\\" + f)
+
+
+def move_protected_back():
     for f in protected_files:
-        file_name = f.replace('\n', "")
-        shutil.copy2(os.path.join(os.getcwd(), "InfernalAdmin\\" + file_name), os.getcwd() + "\\" + file_name)
+        shutil.copy2(os.getcwd() + "\\" + f, os.path.join(os.getcwd(), "InfernalAdmin\\" + f))
 
 if Path(os.path.join(os.getcwd()), 'InfernalAdmin').exists():
     print("Path exists")
     copy_protected()
+    shutil.rmtree(os.path.join(os.getcwd(), 'InfernalAdmin'), onerror=on_rm_error)
+
 else:
     print("Path does not exist")
 
-git_uri = "https://github.com/PeterGibbs/InfernalAdmin.git"
-cmd = "pip install --upgrade --src=" + os.getcwd() + " -e git+https://github.com/PeterGibbs/InfernalAdmin.git@origin/master#egg=InfernalAdmin"
-branch = "Peter-Develop"
-branch_Path = "git+https://github.com/user/repo.git@origin/" + branch + "#egg=InfernalAdmin"
-# subprocess.Popen(["pip","install","--src",os.getcwd(),"-U",branch_Path],shell=True)
-subprocess.call(["git", "clone", git_uri, "--branch", branch], shell=True)
+branch = sys.argv[1]
+print("BRANCH IS :", branch)
+branch_Path = "git+https://github.com/PeterGibbs/InfernalAdmin.git@origin/" + branch + "#egg=InfernalAdmin"
+subprocess.call(["pip", "install", "--src", os.getcwd(), "-e", branch_Path], shell=True)
+
+subprocess.call(["pip", "install", "-U", "-r", os.path.join(os.getcwd(), 'InfernalAdmin/requirements.txt')], shell=True)
+
+move_protected_back()
