5ca786e34f9bf818b34c6486770904ea6d022a0d
BuildTools <pgibbs65@gmail.com>
2019-10-12 18:19:03 -0600

    Changed updater

--- bot_modules/server_control.py
+++ bot_modules/server_control.py
@@ -1,3 +1,4 @@
+from discord.ext import commands
 from discord.ext.commands import Cog
 
 
@@ -6,5 +7,10 @@ class ServerControl(Cog):
 
     def __init__(self, bot):
         self.bot = bot
+    
+    @commands.command()
+    async def mike(self, ctx):
+        myid = '<@229379462968508417>'
+        await ctx.send(myid + ' I already have a welcome function, you should add the picture code')
 def setup(bot):
     bot.add_cog(ServerControl(bot))
