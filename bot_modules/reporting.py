3ed8862256932f9f4c55649d87d280c038a33a3f
BuildTools <pgibbs65@gmail.com>
2019-10-14 18:48:58 -0600

    Started on reporting system

--- bot_modules/reporting.py
+++ bot_modules/reporting.py
@@ -3,10 +3,16 @@ from discord.ext.commands import Cog, command
 
 
 class Reporting(Cog):
-    """The AutoUpdate module contains everything needed to perform git operations on the bot"""
+    """The reporting Module allows users to submit reports to the admins."""
 
     def __init__(self, bot):
         self.bot = bot
+        self.catagory_list = ["admin abuse",
+                              "dispute between users",
+                              "spam",
+                              "bot abuse",
+                              "harrasssment",
+                              "server issue"]
 
 
     @command()
@@ -14,21 +20,57 @@ class Reporting(Cog):
         """->Submits a report to the admins"""
         reporter = ctx.message.author
 
+
         def check(m):
             if isinstance(m.channel, discord.DMChannel):
                 return m.author == reporter
+
 
         if not isinstance(ctx.message.channel, discord.DMChannel):
             await ctx.message.delete()
         dm = await ctx.message.author.send(
             "Please select one of the following categories.\n"
-            "```Admin abuse"
-            "Dispute between users"
-            "Spam"
-            "Bot abuse"
-            "Harrasssment")
+            "```Admin Abuse\n"
+            "Dispute between users\n"
+            "Spam\n"
+            "Bot abuse\n"
+            "Harrasssment\n"
+            "Server Issue```")
         msg = await self.bot.wait_for('message', timeout=120, check=check)
-        dm = await ctx.message.author.send("Good")
+        while msg.content.lower() not in self.catagory_list:
+            await ctx.message.author.send("Invalid category please try again.")
+            msg = await self.bot.wait_for('message', timeout=120, check=check)
+        selected_category = msg.content
+
+        await ctx.message.author.send("Was there an offender?(y/n)")
+        msg = await self.bot.wait_for('message', timeout=120, check=check)
+        offender_id = 0
+        if 'y' in msg.content:
+            await ctx.message.author.send("Please copy their name")
+            msg = await self.bot.wait_for('message', timeout=120, check=check)
+
+        await ctx.message.author.send("Please describe the issue.\n"
+                                      "You can use as many messages as you need\n"
+                                      "You can post screenshots here, but try not to.\n"
+                                      "You will be able to paste messages for evidence in the next step\n"
+                                      "say \"done\" when you are done")
+        report_text = ""
+        while True:
+            msg = await self.bot.wait_for('message', timeout=120, check=check)
+            if msg.content == "done":
+                break
+            else:
+                report_text += msg.content + "\n"
+
+        await ctx.message.author.send("sending report")
+        await ctx.message.author.send("Your report ```" + report_text + "```")
+
+
+
+
+
+
+
 
 
 
