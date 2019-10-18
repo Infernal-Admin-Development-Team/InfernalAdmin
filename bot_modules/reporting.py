4d764f648bebf8b4747609cba0b63ceee61e763f
BuildTools <pgibbs65@gmail.com>
2019-10-17 19:42:22 -0600

    Added more database objects

--- bot_modules/reporting.py
+++ bot_modules/reporting.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext.commands import Cog, command
 
+from database import *
+
 
 class Reporting(Cog):
     """The reporting Module allows users to submit reports to the admins."""
@@ -14,12 +16,27 @@ class Reporting(Cog):
                               "harrasssment",
                               "server issue"]
 
+    async def file_report(self, category, poster_id, offender_id, content, references):
+        server = self.bot.get_guild(CONFIG.server)
+        s = session()
+        p = Report(poster_id=poster_id, offender_id=offender_id, content=content, category=category)
+        # s.add(p)
+        # s.commit()
+        s.close()
+        for c in server.channels:
+            print(c.name)
+            if c.id == CONFIG.reports_category:
+                await server.create_text_channel("test", category=c)
 
     @command()
     async def report(self, ctx):
         """->Submits a report to the admins"""
         reporter = ctx.message.author
-
+        server = self.bot.get_guild(CONFIG.server)
+        offender = None
+        selected_category = None
+        report_text = ""
+        member_name = ""
 
         def check(m):
             if isinstance(m.channel, discord.DMChannel):
@@ -36,25 +53,29 @@ class Reporting(Cog):
             "Bot abuse\n"
             "Harrasssment\n"
             "Server Issue```")
+
         msg = await self.bot.wait_for('message', timeout=120, check=check)
         while msg.content.lower() not in self.catagory_list:
             await ctx.message.author.send("Invalid category please try again.")
             msg = await self.bot.wait_for('message', timeout=120, check=check)
-        selected_category = msg.content
+
+        selected_category = self.catagory_list.index(msg.content.lower())
 
         await ctx.message.author.send("Was there an offender?(y/n)")
         msg = await self.bot.wait_for('message', timeout=120, check=check)
-        offender_id = 0
+
         if 'y' in msg.content:
             await ctx.message.author.send("Please copy their name")
-            msg = await self.bot.wait_for('message', timeout=120, check=check)
+            while True:
+                msg = await self.bot.wait_for('message', timeout=120, check=check)
+
 
         await ctx.message.author.send("Please describe the issue.\n"
                                       "You can use as many messages as you need\n"
                                       "You can post screenshots here, but try not to.\n"
                                       "You will be able to paste messages for evidence in the next step\n"
                                       "say \"done\" when you are done")
-        report_text = ""
+
         while True:
             msg = await self.bot.wait_for('message', timeout=120, check=check)
             if msg.content == "done":
--- database/__init__.py
+++ database/__init__.py
@@ -13,11 +13,38 @@ Base = declarative_base()
 class Report(Base):
     __tablename__ = 'report'
     id = Column(Integer, primary_key=True)
+    category = Column(Integer)
     poster_id = Column(Integer)
+    offender_id = Column(Integer)
+    content = Column(String)
+    references = Column()
 
+
+class Reference(Base):
+    __tablename__ = 'report_reference'
+    reference_id = Column(Integer, primary_key=True)
+    report_id = ForeignKey(Report.id)
+    content = Column(String)
+    attachment = Column(String)
+
+
+class Message(Base):
+    __tablename__ = 'message'
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+    author_id = Column(Integer)
+
+
+class Attachment(Base):
+    __tablename__ = 'message_attachment'
+    id = Column(Integer, primary_key=True)
+    file_path = Column(String)
+    message_id = ForeignKey(Message.id)
+    report = relationship(Report)
+    timestamp = Column(DateTime)
 
 class ReportComment(Base):
-    __tablename__ = 'address'
+    __tablename__ = 'report_comment'
     id = Column(Integer, primary_key=True)
     report_id = ForeignKey(Report.id)
     report = relationship(Report)
